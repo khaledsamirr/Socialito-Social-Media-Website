b127420f52c5069a2443deaa2fc3b21365ff4ad7
Khaled Samir <66645843+khaledsamirr@users.noreply.github.com>
2021-11-19 12:35:27 +0200

    Authentication API Registration

---
+++ Socialito-REST-API/index.js
@@ -0,0 +1,27 @@
+const express =require("express");
+const app= express();
+const mongoose= require("mongoose");
+const dotenv= require("dotenv");
+const helmet= require("helmet");
+const morgan= require("morgan");
+const userRoute=require("./routes/users");
+const authRoute=require("./routes/auth");
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,()=>{
+    console.log("connected to Database successfully!");
+})
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common")); 
+
+app.use("/api/users",userRoute);
+app.use("/api/auth",userRoute);
+
+
+app.listen(8000,()=>{
+    console.log("Backend server is running!");
+})
